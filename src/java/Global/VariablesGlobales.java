dd47ddf625d3d5ab7d3ddecc30454956dd18ff6c
Francisco-Back <fbacks@miumg.edu.gt>
2020-10-23 23:23:46 -0600

    se crea la conexion base de datos

--- src/java/Global/VariablesGlobales.java
+++ src/java/Global/VariablesGlobales.java
@@ -15,17 +15,24 @@ import java.sql.SQLException;
  */
 public class VariablesGlobales {
     public static final String DRIVE_DB="org.postgresql.Driver";
-    public static final String URL_DB="jdbc:postgresql://localhost:5432/postgres";
+    public static final String URL_DB="jdbc:postgresql://localhost:5432/Proyecto3";
     public static final String USER_DB="postgres";
     public static final String PASSWORD_DB="fran*42313380";
     public static Connection conn;
-    static{
+    public Connection getConexion(){
         try{
             Class.forName(DRIVE_DB);
             conn=DriverManager.getConnection(URL_DB,USER_DB,PASSWORD_DB);
+            Object Connection;
+            
+            if(conn!=null){
+                
+                System.out.println("si se conecta");
+            }
         }catch(ClassNotFoundException | SQLException e){
             e.printStackTrace();
             
         }
+        return conn;
     }
 }
