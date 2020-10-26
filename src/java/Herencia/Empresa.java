4bac99cfae8a42228aa9b28ab6ab39809f523266
Francisco-Back <fbacks@miumg.edu.gt>
2020-10-25 23:19:53 -0600

    actuazliazion

--- src/java/Herencia/Empresa.java
+++ src/java/Herencia/Empresa.java
@@ -5,10 +5,59 @@
  */
 package Herencia;
 
+import Clases.Cliente;
+
 /**
  *
  * @author Francisco Back
  */
-public class Empresa {
-    
+public class Empresa extends Cliente{
+        private String Dpi;
+    private int Descuento;
+
+    public Empresa(){
+        
+    }
+// se crea el metodo constructor con los atibutos de la clase super
+    public Empresa(String Dpi, int Descuento, int IdCliente, String NombreCliente, String Codigo, String Empresa, String Funcion, String TipoCliente) {
+        super(IdCliente, NombreCliente, Codigo, Empresa, Funcion, TipoCliente);
+        this.Dpi = Dpi;
+        this.Descuento = Descuento;
+
+    }
+
+  
+
+    public Empresa(int IdCliente) {
+        super(IdCliente);
+
+    }
+
+    // se crea lso metodos get y set
+    public String getDpi() {
+        return Dpi;
+    }
+
+    public void getDpi(String Dpi) {
+        this.Dpi = Dpi;
+    }
+
+    public int getDescuento() {
+        return Descuento;
+    }
+
+    public void setDescuento(int Descuento) {
+        this.Descuento = Descuento;
+    }
+
+    public void setDpi(String Dpi) {
+        this.Dpi = Dpi;
+    }
+
+//se crea el metodo toString
+    @Override
+    public String toString() {
+        return "Empresa{" + "Contacto=" + Dpi + ", Descuento=" + Descuento + '}' + super.toString();
+    }
+
 }
