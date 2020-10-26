af56d3667348ca859d33a5940dc6a688f06e4f5d
Francisco-Back <fbacks@miumg.edu.gt>
2020-10-25 23:19:31 -0600

    actuazliazion

--- src/java/Clases/Producto.java
+++ src/java/Clases/Producto.java
@@ -5,10 +5,100 @@
  */
 package Clases;
 
+import Global.Utileria;
+
 /**
  *
  * @author Francisco Back
  */
 public class Producto {
+    // se crea los atributos
+
+    public static int correlativoCodigo = 2000;
+    private int IdProducto;
+    private String NombreParte;
+    private String Codigo;
+    private String Marca;
+    private String Funcion;
+    private int precio;
+
+    // se instancian los metodos constructores   
+
+    public Producto(int IdProducto) {
+        this.IdProducto = 0;
+    }
+
+    public Producto() {
+        this.IdProducto = 0;
+        this.NombreParte = "";
+        this.Codigo = "";
+        this.Marca = "";
+        this.Funcion = "";
+        this.precio = 0;
+    }
+
+    public Producto(int IdProducto, String NombreParte, String Codigo, String Marca, String Funcion, int precio) {
+        this.IdProducto = correlativoCodigo++;
+        this.NombreParte = NombreParte;
+        this.Codigo = Codigo;
+        this.Marca = Marca;
+        this.Funcion = Funcion;
+        this.precio = precio;
+    }
+// se crean lso metodos get y set de la clase
+    public int getIdProducto() {
+        return IdProducto;
+    }
+
+    public void setIdProducto(int IdProducto) {
+        this.IdProducto = IdProducto;
+    }
+
+    public String getNombreParte() {
+        return NombreParte;
+    }
+
+    public void setNombreParte(String NombreParte) {
+        this.NombreParte = NombreParte;
+    }
+
+    public String getCodigo() {
+        return Codigo;
+    }
+
+    public void setCodigo(String Codigo) {
+        this.Codigo = Codigo;
+    }
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public void setMarca(String Marca) {
+        this.Marca = Marca;
+    }
+
+    public String getFuncion() {
+        return Funcion;
+    }
+
+    public void setFuncion(String Funcion) {
+        this.Funcion = Funcion;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+// se sobreescribe el metodo toString
+    @Override
+    public String toString() {
+        Utileria utileria = new Utileria();
+        return "[" + utileria.getNombreClase(this.getClass().getSimpleName()) + "]" + "IdProducto=" + IdProducto + ", NombreParte=" + NombreParte + ", Codigo=" + Codigo + ", Marca=" + Marca + ", Funcion=" + Funcion  + ",Precio" + precio;
+    }
+
     
 }
