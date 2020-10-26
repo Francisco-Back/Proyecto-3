31cee751077c3d37fb01ff55d88f93829cf0fbc1
Francisco-Back <fbacks@miumg.edu.gt>
2020-10-25 23:18:36 -0600

    se crea los la referencia

--- src/java/Global/UsuarioDAO.java
+++ src/java/Global/UsuarioDAO.java
@@ -5,49 +5,156 @@
  */
 package Global;
 
+import Clases.Cliente;
 import Clases.Usuario;
+import Herencia.Empresa;
+import Herencia.Individual;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import javax.naming.spi.DirStateFactory;
 
 /**
  *
  * @author Francisco Back
  */
-public class UsuarioDAO implements Validar{
+public class UsuarioDAO implements Validar {
+
     Connection conn;
-    VariablesGlobales cn=new VariablesGlobales();
+    VariablesGlobales cn = new VariablesGlobales();
     PreparedStatement ps;
     ResultSet rs;
-    String t;
-     
-        
+    int r;
+
     @Override
-  
+
     public int Validar(Usuario user) {
-         int r=0;
-        String sql="Select * from USUARIOS where USUARIO=? and CONTRASENA=?";
-        try{
-           conn=cn.getConexion();
-           ps=conn.prepareStatement(sql);
-           ps.setString(1, user.getUsuario());
-           ps.setString(2, user.getPassword());
-           rs=ps.executeQuery();
-           while(rs.next()){
-               r=r+1;
-               user.setUsuario(rs.getString("USUARIO"));
-               user.setPassword(rs.getString("CONTRASENA"));
-           }
-           if(r==1){
-              return 1;
-           }else{
-               return 0;
-           }
-        }catch(Exception e){
+        int r = 0;
+        String sql = "Select * from USUARIOS where USUARIO=? and CONTRASENA=?";
+        try {
+            conn = cn.getConexion();
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, user.getUsuario());
+            ps.setString(2, user.getPassword());
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                r = r + 1;
+                user.setUsuario(rs.getString("USUARIO"));
+                user.setPassword(rs.getString("CONTRASENA"));
+            }
+            if (r == 1) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return 0;
     }
-    
+
+   public List Lista() {
+        String sql = "SELECT * FROM CLIENTE ";
+        List<Empresa> lista = new ArrayList<>();
+        try {
+            conn = cn.getConexion();
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Empresa em = new Empresa();
+                em.setIdCliente(rs.getInt(1));
+                em.setDpi(rs.getString(2));
+                em.setNombreCliente(rs.getString(3));
+                em.setCodigo(rs.getString(4));
+                em.setEmpresa(rs.getString(5));
+                em.setFuncion(rs.getString(6));
+                em.setDescuento(rs.getInt(7));
+                em.setTipoCliente(rs.getString(8));
+                lista.add(em);
+            }
+        } catch (Exception e) {
+
+        }
+        return lista;
+    }
+  
+
+    public int Agregar(Empresa h) {
+        String sql = "INSET INTO ClIENTE(idcliente,dpi,nombrecliente,codigo,empresa,funcion,descuento,Cliente1)VALUES()";
+        try {
+            conn = cn.getConexion();
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, h.getIdCliente());
+            ps.setString(2, h.getDpi());
+            ps.setString(3, h.getNombreCliente());
+            ps.setString(4, h.getCodigo());
+            ps.setString(5, h.getEmpresa());
+            ps.setString(6, h.getFuncion());
+            ps.setInt(7, h.getDescuento());
+            ps.setString(8, h.getTipoCliente());
+            ps.executeUpdate();
+
+        } catch (Exception e) {
+
+        }
+        return r;
+    }
+
+    public Empresa ListarId(int IdCliente) {
+        Empresa emp = new Empresa();
+        String sql = "SELECT * FROM CLIENTE WHERE IdCliente=" + IdCliente;
+        try {
+            conn = cn.getConexion();
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                emp.setDpi(rs.getString(2));
+                emp.setNombreCliente(rs.getString(3));
+                emp.setCodigo(rs.getString(4));
+                emp.setEmpresa(rs.getString(5));
+                emp.setFuncion(rs.getString(6));
+                emp.setDescuento(rs.getInt(7));
+                emp.setTipoCliente(rs.getString(8));
+            }
+        } catch (Exception e) {
+
+        }
+        return emp;
+    }
+
+    public int Actulizar(Empresa h) {
+        String sql = "UPDATE ClIENTE SET (dpi=?,nombrecliente=?,codigo=?,empresa=?,funcion=?,descuento=?,Cliente1=? WHERE idcliente=?)";
+        try {
+            conn = cn.getConexion();
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, h.getIdCliente());
+            ps.setString(2, h.getDpi());
+            ps.setString(3, h.getNombreCliente());
+            ps.setString(4, h.getCodigo());
+            ps.setString(5, h.getEmpresa());
+            ps.setString(6, h.getFuncion());
+            ps.setInt(7, h.getDescuento());
+            ps.setString(8, h.getTipoCliente());
+            ps.executeUpdate();
+
+        } catch (Exception e) {
+
+        }
+        return r;
+    }
+
+    public void Eliminar(int IdCliente) {
+        String sql = "DELETE FROM CLIENTE WHERE IdCliente= " + IdCliente;
+        try {
+            conn = cn.getConexion();
+            ps = conn.prepareStatement(sql);
+            ps.executeUpdate();
+        } catch (Exception e) {
+
+        }
+
+    }
 }
