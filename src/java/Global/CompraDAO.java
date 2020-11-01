a78bc709dc7cf0293b378655c089ecb05fc0665d
Francisco-Back <fbacks@miumg.edu.gt>
2020-11-01 14:26:25 -0600

    actualizacion final

--- src/java/Global/CompraDAO.java
+++ src/java/Global/CompraDAO.java
@@ -5,6 +5,13 @@
  */
 package Global;
 
+import Clases.Compra;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Francisco Back
@@ -16,46 +23,48 @@ public class CompraDAO {
     ResultSet rs;
     int r;
 
-        public Producto Buscar(int codigo){
-        Producto ty= new Producto ();
+        public Compra Buscar(int codigo){
+        Compra co= new Compra ();
         String sql="SELECT * FROM PRODUCTO WHERE IdProducto="+codigo;
         try{
             conn=cn.getConexion();
             ps=conn.prepareStatement(sql);
             rs=ps.executeQuery();
             while(rs.next()){
-                 ty.setIdProducto(rs.getInt(1));
-                ty.setNombreParte(rs.getString(2));
-                ty.setCodigo(rs.getString(3));
-                ty.setMarca(rs.getString(4));
-                ty.setFuncion(rs.getString(5));
-                ty.setPrecio(rs.getInt(6));
-                ty.setExistencia(rs.getInt(7));
+           /*      co.setIdProducto(rs.getInt(1));
+                co.setNombreParte(rs.getString(2));
+                co.setCodigo(rs.getString(3));
+                co.setMarca(rs.getString(4));
+                co.setFuncion(rs.getString(5));
+                co.setPrecio(rs.getInt(6));
+                co.setExistencia(rs.getInt(7));*/
             }
         }catch(Exception e){
             
         }
-        return ty;
+        return co;
     }
 
-   public List Lista() {
-        String sql = "SELECT * FROM PRODUCTO";
-        List<Producto> listap = new ArrayList<>();
+   public List Listac() {
+        String sql = "SELECT * FROM Compra";
+        List<Compra> listap = new ArrayList<>();
         try {
             conn = cn.getConexion();
             ps = conn.prepareStatement(sql);
             rs = ps.executeQuery();
             while (rs.next()) {
-                Producto pro = new Producto();
-                pro.setIdProducto(rs.getInt(1));
-                pro.setNombreParte(rs.getString(2));
-                pro.setCodigo(rs.getString(3));
-                pro.setMarca(rs.getString(4));
-                pro.setFuncion(rs.getString(5));
-                pro.setPrecio(rs.getInt(6));
-                pro.setExistencia(rs.getInt(7));
-               
-                listap.add(pro);
+               Compra cop = new Compra();
+             cop.setIdproducto(rs.getInt(1));
+             cop.setIdcli(rs.getInt(2));
+             cop.setIdOrden(rs.getInt(3));
+             cop.setFechaOrden(rs.getDate(4));
+             cop.setPrecioEnvio(rs.getDouble(5));
+             cop.setTotal(rs.getDouble(6));
+             cop.setEnvio(rs.getString(7));
+             cop.setEstado(rs.getString(8));
+             cop.setDiasEnvio(rs.getInt(9));
+             cop.setDescripcion(rs.getString(10));
+                listap.add(cop);
             }
         } catch (Exception e) {
 
@@ -65,7 +74,7 @@ public class CompraDAO {
    
    public int contador() {
        int contador = 0;
-        String sql = "SELECT * FROM PRODUCTO";
+        String sql = "SELECT * FROM COMPRA";
         try {
             conn = cn.getConexion();
             ps = conn.prepareStatement(sql);
@@ -79,19 +88,19 @@ public class CompraDAO {
         return contador;
     }    
 
-    public int Agregar(Producto pro) {
-        String sql = "INSERT INTO PRODUCTO(idproducto,nombreparte,codigo,marca,funcion,precio,existencia)VALUES(?,?,?,?,?,?,? )";
+    public int Agregar(Compra comp) {
+        String sql = "INSERT INTO COMPRAVALUES(?,?,?,?,?,?,?,?,?,?)";
         try {
-            conn = cn.getConexion();
+          /*  conn = cn.getConexion();
             ps = conn.prepareStatement(sql);
-            ps.setInt(1, pro.getIdProducto());
-            ps.setString(2, pro.getNombreParte());
-            ps.setString(3, pro.getCodigo());
-            ps.setString(4, pro.getMarca());
+            ps.setInt(1, comp.getIdproducto());
+            ps.setInt(2, comp.getIdcli());
+            ps.setInt(3, comp.getIdOrden());
+            ps.setDate(4, comp.get());
             ps.setString(5, pro.getFuncion());
             ps.setInt(6, pro.getPrecio());
             ps.setInt(7, pro.getExistencia());
-            ps.executeUpdate();
+            ps.executeUpdate();*/
 
         } catch (Exception e) {
 
@@ -100,20 +109,20 @@ public class CompraDAO {
     }
     
 
-    public Producto ListarId(int id) {
-        Producto pro = new Producto();
+    public Compra ListarId(int id) {
+        Compra pro = new Compra();
         String sql = "SELECT * FROM PRODUCTO WHERE IdProducto=" + id;
         try {
             conn = cn.getConexion();
             ps = conn.prepareStatement(sql);
             rs = ps.executeQuery();
             while (rs.next()) {
-                pro.setNombreParte(rs.getString(2));
+              /*  pro.setNombreParte(rs.getString(2));
                 pro.setCodigo(rs.getString(3));
                 pro.setMarca(rs.getString(4));
                 pro.setFuncion(rs.getString(5));
                 pro.setPrecio(rs.getInt(6));
-                pro.setExistencia(rs.getInt(7));
+                pro.setExistencia(rs.getInt(7));*/
                
             }
         } catch (Exception e) {
@@ -122,20 +131,20 @@ public class CompraDAO {
         return pro;
     }
 
-    public int Actulizar(Producto pro) {
+    public int Actulizar(Compra comp) {
         String sql = "UPDATE PRODUCTO SET nombreparte=?,codigo=?,marca=?,funcion=?,precio=?,existencia=? WHERE IdProducto=?";
         try {
             conn = cn.getConexion();
             ps = conn.prepareStatement(sql);
           
-            ps.setString(1, pro.getNombreParte());
+         /*   ps.setString(1, pro.getNombreParte());
             ps.setString(2, pro.getCodigo());
             ps.setString(3, pro.getMarca());
             ps.setString(4, pro.getFuncion());
             ps.setInt(5, pro.getPrecio());
             ps.setInt(6, pro.getExistencia());
             ps.setInt(7, pro.getIdProducto());
-            ps.executeUpdate();
+            ps.executeUpdate();*/
 
         } catch (Exception e) {
 
